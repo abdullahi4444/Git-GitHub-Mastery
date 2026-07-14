4c5958ee0148390dcc44b6b155b3583885c894b4
abdullahi4444 <abdalapoi223@gmail.com>
2026-07-14 20:30:00 +0300

    44 - Update script.js with JavaScript logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Initializing script.js - Step 34");
+console.log("Initializing script.js - Step 44");
 function greet() {
-    return "Hello from Git practice! (Commit 34)";
+    return "Hello from Git practice! (Commit 44)";
 }
